8b37fb890bafe58f7e4176a02fb427d42d139baa
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-06 12:45:24 -0500

    added hacker news api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import "./App.css";
 import React from "react";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://react.js.org",
-    author: "Jordan Walker",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
-// const getAsyncStories = () =>
-//   new Promise((resolve, reject) => setTimeout(reject, 2000));
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -68,11 +46,12 @@ const App = () => {
 
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((r) => r.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
