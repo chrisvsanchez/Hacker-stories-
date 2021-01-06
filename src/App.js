03828d23469c030b6ffea1af5c6f2787a27eff25
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-06 14:49:43 -0500

    updated feature to server-side search by dynamically changing the fetch address according to searchTerm

--- src/App.js
+++ src/App.js
@@ -45,8 +45,9 @@ const App = () => {
   });
 
   React.useEffect(() => {
+    if (!searchTerm) return;
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((r) => r.json())
       .then((result) => {
         dispatchStories({
@@ -55,7 +56,7 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({ type: "REMOVE_STORY", payload: item });
@@ -83,7 +84,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
