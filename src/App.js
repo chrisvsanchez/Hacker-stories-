23a796045e2c760393415ad0b86fafe16dc68709
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-05 21:42:28 -0500

    added inline handler to item component passed down from parent to remove item on list

--- src/App.js
+++ src/App.js
@@ -1,70 +1,113 @@
 import "./App.css";
 import React from "react";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://react.js.org",
+    author: "Jordan Walker",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
+};
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://react.js.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-  const useSemiPersistentState = (key) => {
-    const [value, setValue] = React.useState(localStorage.getItem(key) || "");
-    React.useEffect(() => {
-      localStorage.setItem(key, value);
-    }, [value, key]);
-    return [value, setValue];
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const [stories, setStories] = React.useState(initialStories);
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
   };
-  const [value, setValue] = useSemiPersistentState();
   const handleSearch = (e) => {
-    setValue(e.target.value);
+    setSearchTerm(e.target.value);
     // localStorage.setItem("search", e.target.value);
   };
   const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(value.toLowerCase())
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel id="search" search={value} onInputChange={handleSearch}>
+      <InputWithLabel
+        id="search"
+        search={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} {...item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ title, url, author, num_comments, points }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        {/* <button type="button" onClick={onRemoveItem.bind(null, item)}> */}
+        Dismiss
+      </button>
+    </span>
   </div>
 );
-const InputWithLabel = ({ id, label, value, onInputChange, type = "text" }) => {
+
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  onInputChange,
+  type = "text",
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} value={value} onChange={onInputChange} />
+      <label htmlFor={id}>{children}</label>
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
     </>
   );
 };
